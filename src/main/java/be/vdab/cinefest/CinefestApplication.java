aff37d22889730d3134e754999f0ae76862708bb
yuliyaBir <zhivo4ka@gmail.com>
2023-06-27 14:19:14 +0200

    Initial commit

---
+++ src/main/java/be/vdab/cinefest/CinefestApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.cinefest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinefestApplication.class, args);
+    }
+
+}
