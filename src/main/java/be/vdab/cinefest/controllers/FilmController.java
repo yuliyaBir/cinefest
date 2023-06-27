cd7721bebf0c0f318a6d944c29dbd17467f6d2b7
yuliyaBir <zhivo4ka@gmail.com>
2023-06-27 15:50:22 +0200

    GET

---
+++ src/main/java/be/vdab/cinefest/controllers/FilmController.java
@@ -0,0 +1,17 @@
+package be.vdab.cinefest.controllers;
+
+import be.vdab.cinefest.services.FilmService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilmController {
+    private final FilmService filmService;
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+    @GetMapping("films/aantalVrijePlaatsen")
+    public long findAantalVrijePlaatsen(){
+        return filmService.findAantalVrijePlaatsen();
+    }
+}
